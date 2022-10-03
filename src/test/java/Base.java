48abfc263d06abd462bacceebc27b8aa3a6a4cc7
mehediabir1 <mehedi.abir2@gmail.com>
2022-10-03 15:17:22 +0600

    2

---
+++ src/test/java/Base.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.file.Watchable;
+
+public class Base {
+
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        ChromeOpen();
+    }
+
+    public static void ChromeOpen(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public static void ChromeClose(){
+        driver.quit();
+    }
+
+    public static void GetUrl(String URL){
+        driver.get(URL);
+    }
+
+    public static WebElement findById(String Locator){
+        return driver.findElement(By.id(Locator));
+    }
+
+    public static WebElement findByClass(String Locator){
+        return driver.findElement(By.className(Locator));
+    }
+
+    public static WebElement findByCSS(String Locator){
+        return driver.findElement(By.cssSelector(Locator));
+    }
+
+    public static WebElement findByXpath(String Locator){
+        return driver.findElement(By.xpath(Locator));
+    }
+
+    public static WebElement findByName(String Locator){
+        return driver.findElement(By.name(Locator));
+    }
+
+    public static void SleepTime(int time) throws InterruptedException {
+        Thread.sleep(time);
+    }
+
+}
